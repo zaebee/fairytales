c51a4ba5a1b4a85be169cf6bda095dc0f9f8ca6d
zaebee <zaebuntu@gmail.com>
2022-12-11 10:51:39 +0100

    Added base service for Tale generator.

---
+++ backend/app/app/services/cohere.py
@@ -0,0 +1,170 @@
+"""Implements tales generator on Cohere service."""
+
+import re
+import os
+import cohere
+import pandas as pd
+import tales as base_tales
+
+COHERE_KEY = 'onk1aMdIZZ4P86E99JlD095UEVH7LfIUwwmEF7KQ'
+STABILITY_KEY = 'sk-kKDdwGtHoPO4kiOSWQck3D1TEaBRRAVMUPhdKyHUN9A0DVH3'
+
+# Paste your API key here. Remember to not share it publicly
+os.environ['COHERE_KEY'] = COHERE_KEY
+
+
+class TalePrompt:
+    """Generates prompts for tale structure and text."""
+
+    HEAD = ('Examples of breakdowns of story into Vladimir Propp\'s '
+    '"Morphology of a fairy tale" structutre.\n')
+
+    SAMPLE = 'Example {counter}.\n{text}\n\n<scenes>\n{predict}\n\n<end>\n'
+    SAMPLE_PREDICT = 'Example {counter}. \n{text}\n\n<scenes>'
+
+    HEROES = ('Example {counter}.\n{text}\nCharacters and descriptions:'
+    '\n{heroes}\n<end>')
+    HEROES_PREDICT = 'Example {counter}.\n{text}\nCharacters and descriptions:'
+
+    def __init__(self, line, as_tale=None, **kwargs):
+        self.client = None
+        self.heroes = {}
+        self.structures = {}
+        self.line = line
+        self.as_tale = as_tale
+        self.tales = kwargs.get('tales', base_tales.TALES)
+
+    @classmethod
+    async def create(cls, line, as_tale: str = None, **kwargs):
+        """Initializes async client."""
+        self = cls(line, as_tale, **kwargs)
+        self.client = await cohere.AsyncClient.create(COHERE_KEY)
+        return self
+
+    async def generate(
+        self, prompt: str, model: str = 'xlarge',
+        stop_sequences: list[str] = None, **kwargs
+    ) -> str:
+        """Generates sequence from given prompt."""
+        params = {
+            'return_likelihoods': 'GENERATION',
+            'stop_sequences': stop_sequences or ['<end>'],
+            'max_tokens': kwargs.get('max_tokens', 50),
+            'num_generations': kwargs.get('', 5),
+            'temperature': kwargs.get('temperature', 0.7),
+        }
+        prediction = await self.client.generate(
+            model=model, prompt=prompt, **params)
+        gens = []
+        likelihoods = []
+        for gen in prediction.generations:
+            gens.append(gen.text)
+
+            sum_likelihood = 0
+            for token in gen.token_likelihoods:
+                sum_likelihood += token.likelihood
+                likelihoods.append(sum_likelihood)
+
+        pd.options.display.max_colwidth = 200
+        data = pd.DataFrame({'generation':gens, 'likelihood': likelihoods})
+        data = data.drop_duplicates(subset=['generation'])
+        data = data.sort_values('likelihood', ascending=False, ignore_index=True)
+        return data
+
+    def prompt_structure(self, text) -> str:
+        """Generates prompt to get tale structure."""
+        output = [self.HEAD]
+        for counter, tale in enumerate(self.tales, 1):
+            sample = self.SAMPLE.format(
+                counter=counter, text=tale['summary'], predict=tale['struct'])
+            output.append(sample)
+        predict = self.SAMPLE_PREDICT.format(
+            counter=len(self.tales) + 1, text=text)
+        output.append(predict)
+        return '\n'.join(output)
+
+    def prompt_text_as_tale(self, text, as_tale) -> str:
+        """Generates prompt to get tale full text."""
+        tales = [tale for tale in self.tales if tale['name'] == as_tale]
+        output = [self.HEAD]
+        if not tales:
+            return ''
+        for counter, tale in enumerate(tales, 1):
+            summary = '\n'.join([tale['summary'], tale['struct']])
+            sample = self.SAMPLE.format(
+                counter=counter, text=summary, predict=tale['text'])
+            output.append(sample)
+        predict = self.SAMPLE_PREDICT.format(counter=len(tales) + 1, text=text)
+        output.append(predict)
+        return '\n'.join(output)
+
+    def prompt_structure_as_tale(self, text, as_tale) -> str:
+        """Generates prompt to predict tale structure similar as given tale."""
+        tales = [tale for tale in self.tales if tale['name'] == as_tale]
+        output = [self.HEAD]
+        if not tales:
+            return ''
+        for counter, tale in enumerate(tales, 1):
+            sample = self.SAMPLE.format(
+                counter=counter, text=tale['text'], predict=tale['struct'])
+            output.append(sample)
+        predict = self.SAMPLE_PREDICT.format(counter=len(tales) + 1, text=text)
+        output.append(predict)
+        return '\n'.join(output)
+
+    def prompt_heroes(self, text: str, as_tale: str = None) -> str:
+        """Builds prompt to get heroes description."""
+        tales = self.tales
+        if as_tale:
+            tales = [tale for tale in self.tales if tale['name'] == as_tale]
+        output = []
+        for counter, tale in enumerate(tales, 1):
+            output.append(self.HEROES.format(
+                counter=counter, text=tale['summary'], heroes=tale['heroes']))
+        predict = self.HEROES_PREDICT.format(counter=len(tales) + 1, text=text)
+        output.append(predict)
+        return '\n'.join(output)
+
+    async def get_heroes(self, as_tale: str = None, max_tokens: int = 500):
+        """Generates heroes names and descriptions."""
+        prompt = self.prompt_heroes(self.line, as_tale)
+        result = await self.generate(
+            prompt, num_generations=3, temperature=0.618,
+            max_tokens=max_tokens)
+        for idx, gen in enumerate(result['generation'].values):
+            descriptions = re.findall(
+                r'\<description\>\s(.*?)\s<stop>', gen, re.DOTALL)
+            names = re.findall(
+                r'\<character\>\s(.*?)\s<description>', gen, re.DOTALL)
+            self.heroes[idx] = {'names': names, 'descriptions': descriptions}
+        return self.heroes
+
+    async def get_structure(self, heroes: int = None, max_tokens: int = 500):
+        """Generates tale structure gor given heroes."""
+        text = [self.line]
+        if heroes is not None:
+            text.append('\n'.join(self.heroes[heroes]['descriptions']))
+        prompt = self.prompt_structure('\n'.join(text))
+        output = await self.generate(
+            prompt, num_generations=3, temperature=0.88, max_tokens=max_tokens)
+        for idx, gen in enumerate(output['generation'].values):
+            matched = re.search(r'(1\).*?)\n\n', gen, re.DOTALL)
+            if matched:
+                self.structures[idx] = matched.group()
+        return self.structures
+
+    async def get_tale(
+        self, as_tale: str, structure: int = None, heroes: int = None,
+        max_tokens: int = 500):
+        """Generates final tale text for given heroes and structure."""
+        text = [self.line]
+        if heroes is not None:
+            text.append('\n'.join(self.heroes[heroes]['descriptions']))
+        if structure is not None:
+            text.append(self.structures[structure])
+        prompt = self.prompt_text_as_tale('\n'.join(text), as_tale)
+        if not prompt:
+            return ''
+        output = await self.generate(
+            prompt, num_generations=3, temperature=0.88, max_tokens=max_tokens)
+        return output
---
+++ backend/app/app/services/tales.py
@@ -0,0 +1,170 @@
+TALES = [{
+    'name': 'MARY',
+    'heroes': '',
+    'summary': (
+        'A fairy tale about a daughter who was cared for by the virgin. '
+        'The girl deceived her, and the Mother of God punished her for it.'),
+    'struct': '''
+1) Absence (the Virgin Mary has a long way to go. So, she gives the girls the keys to the thirteen gates of heaven)
+2) Prohibition (You can do twelve doors, but I forbid you to open the thirteenth door! Do not open it, otherwise you will be unhappy!)
+3) Violation (Once, when all the angels left, out of curiosity, the girl nevertheless opened the thirteenth door)
+4) Finding out (On her return, the Ever-Virgin looked into her eyes and asked: “Have you unlocked the thirteenth door too?” “No”)
+5) Issuance (Virgin Mary demanded the keys back)
+6) A catch (the Virgin Mary found out about the deception and said: “You disobeyed me and, in addition, you lied, and therefore you are not worthy to stay in heaven anymore!”)
+7) Aiding (The King continued: “Would you like to come with me to my castle?” To this she answered only with a slight nod of her head)
+8) Wrecking (the Blessed Virgin took three children, wanting to hear the truth and repentance)
+9) Mediation (I am the Virgin Mary, the mother of the infant Christ. You are poor, burdened with need. Bring me your child: I will take him with me, I will be his mother and take care of him)
+10) Sending (The woodcutter obeyed, brought his child and handed him to the Virgin Mary, who took him to heaven with her)
+11) Victory (The Queen repented that she opened the door. Then the voice returned to her, and she loudly confessed to Mary what she had done. And at the same moment it rained from heaven and extinguished the flame. The Virgin Mary descended to earth with her newborn daughter)''',
+    'text': '''
+A poor woodcutter and his wife had a three-year-old daughter that they could not feed.
+The Virgin Mary appeared to the woodcutter and promised to take care of the child, so they gave her the child.
+She grew up happily in Heaven. One day the Virgin had to go on a journey and gave the girl keys,
+telling her she could open twelve doors but not the thirteenth. She opened the first twelve and found
+the Apostles behind them. Then she opened the thirteenth door. Behind it was the Trinity, and her finger
+was stained with gold. She tried to hide it, lying three times, and the Virgin Mary said she could no longer remain for her disobedience and lying.
+
+She fell asleep and woke to find herself in a forest. Lamenting her misfortune, she lived in a hollow tree,
+ate wild plants, and tore all her clothing until she was naked. One day, a king found her looking beautiful
+but incapable of speech. He took her home and married her.
+
+A year later, she had a son. The Virgin Mary appeared and demanded that she confess to having opened the door.
+She lied again, the Virgin took her son, and the people whispered that she had killed and eaten the child.
+In another year, she had another son, and it went as before. The third year, she had a daughter, and the Virgin Mary
+took her to heaven and showed her her sons, but she would not confess. This time, the king could not restrain
+his councilors, and the queen was condemned to death. When she was brought to the stake, she relented and wished
+she could confess before she died. The Virgin Mary brought back her children, restored her the power of speech,
+and gave her happiness the rest of her life.'''
+}, {
+    'name': 'BUN',
+    'heroes': '''<character> Luke Skywalker <description> Luke Skywalker is the hero. A naive farm boy, he will discover
+special powers under the guidance of mentor Ben Kenobi. <stop>
+<character> Ben Kenobi <description> Ben Kenobi is the mentor figure. A recluse Jedi warrior, he will
+take Luke Skywalker as apprentice. <stop>
+<character> Darth Vader <description> Darth Vader is the antagonist. As a commander of the evil Galactic
+Empire, he controls space station The Death Star. <stop>
+<character> Princess Leia <description> Princess Leia is a feisty and brave leader of the Rebellion. She
+holds the plans of the Death Star. She will become Luke's friend. <stop>
+<character> Han Solo <description> Han Solo is a brash mercenary space pilot of the Millenium Falcon and
+a friend of Chebacca. He will take Luke on his spaceship. <stop>
+<character> Chewbacca <description> Chewbacca is a furry and trustful monster. He is a friend of Han
+Solo and a copilot on the Millemium Falcon. <stop>''',
+    'summary': (
+        'A fairy tale about a bun that ran away from grandparents, '
+        'ran away from all animals, but the fox ate it.'),
+    'struct': '''
+1) Absence (The old man wants to eat the bun)
+2) Prohibition (The old woman forbids the bun to go out to the windowsill)
+3) Violation (The bun goes out to the windowsill)
+4) Finding out (The hare says he wants to eat the bun)
+5) Extradition (The bun says he ran away from grandfather, grandmother, hare and wolf)
+6) Trick (The fox offers to hear the bun sing, and to eat him at the same time)
+7) Aiding (The bun agrees to sing for the fox)
+8) Wrecking (The bun sings and the fox eats him)
+9) Mediation (The bun runs away from home and meets the hare, wolf, bear, fox)
+10) Departure (The bun leaves home and runs along the road)
+11) Victory (The bun reaches the fox, the fox eats him)
+12) Elimination of misfortune or shortage (The fox eats the bun)''',
+    'text': '''
+One day the old man says to his wife ‘Please, bake me a bun’. The old
+woman takes some flour, some soar cream, some butter and some water,
+and makes a bun. She puts it on the windowsill to cool.
+But the bun cannot sit on the windowsill! It jumps from the windowsill to the
+bench, from the bench to the floor, from the floor to the door, and runs
+away.
+The bun runs along the road and meets a hare. ‘Little bun, little bun, I want
+to eat you!’ says the hare. ‘I ran away from Grandfather, I ran away from
+Grandmother. And I can run away from you, little hare!’ says the bun and
+runs away.
+The bun runs along the road and meets a wolf. ‘Little bun, little bun, I want
+to eat you!’ says the wolf. ‘I ran away from Grandfather, I ran away from
+Grandmother, I ran away from the hare. And I can run away from you, grey
+wolf!’ says the bun and runs away.
+The bun runs along the road and meets a bear. ‘Little bun, little bun, I want
+to eat you!’ says the bear. ‘I ran away from Grandfather, I ran away from
+Grandmother, I ran away from the hare, I ran away from the wolf. And I can
+run away from you, big bear!’ says the bun again and runs away.
+The bun runs along the road and meets a fox. ‘Little bun, little bun, I want to
+eat you!’ says the fox. ‘I ran away from Grandfather, I ran away from
+Grandmother, I ran away from the hare, I ran away from the wolf, I ran
+away from the bear. And I can run away from you, old fox!’
+‘What a nice song!’ says the fox. ‘But little bun, I’m old and I cannot hear
+you well. Sit on my nose and sing your song again.’ The bun jumps on the
+fox’s nose and … the fox eats it!'''
+
+}, {
+    'name': 'RED_HOOD',
+    'heroes': '',
+    'summary': (
+        'The tale tells of a young girl in a red cap who walks through a dark '
+        'forest to her grandmother\'s house. When she arrives, she discovers '
+        'that the wolf has taken her grandmother\'s place, trying to trick '
+        'the girl and eventually eat her.'),
+    'struct': '''
+1) Absence (Little Red Riding Hood leaves her house for her grandmother)
+2) Prohibition (Mother punishes Little Red Riding Hood so that she, going to her grandmother, does not leave the path)
+3) Violation (Little Red Riding Hood leaves the path to collect flowers)
+4) Finding out (The wolf asks Little Red Riding Hood where she is going and where her grandmother lives)
+5) Extradition (Little Red Riding Hood tells the wolf where her grandmother lives and why she goes to her)
+6) Trick (the wolf pretends to be kind and offers Little Red Riding Hood to collect flowers in order to get to the grandmother first. Then, he deceives the grandmother pretending to be Little Red Riding Hood, and then he deceives Little Red Riding Hood, pretending to be grandmother)
+7) Aiding (Little Red Riding Hood gives in to deception and begins to collect flowers, thereby helping the wolf)
+8) Wrecking (The wolf gets to the grandmother, swallows her, dresses himself in her clothes, thereby making a substitution. Further, he swallows Little Red Riding Hood herself)
+9) Mediation (Mother sends Little Red Riding Hood to Grandma)
+10) Departure (Little Red Riding Hood leaves her house and goes to her grandmother)
+11) Victory (The hunter cuts the belly of the wolf, and then they all stuff the belly of the wolf with stones and he dies)
+12) Elimination of misfortune or shortage (Little Red Riding Hood and grandmother are released, grandmother eats the gifts brought by her)''',
+    'text': '''
+Once upon a time, there was a little girl called Little Red Riding Hood.
+One day, her mother said: “Little Red Riding Hood, take this basket full of cakes
+to your grandmother, she is ill. Don’t get distracted on the way, the forest is dangerous and there is a wolf around.
+“Yes Mum!” – Said Little Red Riding hhood.
+foto caperucita roja
+Little Red Riding Hood walked happily to her grandmother’s house, but suddenly, a wolf appeared.
+–          Where are you going, Little Red Riding Hood?
+–          To my granny´s house, to give her these cakes.
+The wolf convinced the girl to take a longer path while he took the short path, so
+he could arrive before her and eat her grandmother. Then he ate Little red Riding Hood.
+Later, after eating them, the wolf fell asleep beside the river. Suddenly, a woodcutter
+saw him and took the little girl and her granny out of the wolf´s stomach and saved their lives.
+Then he filled the wolf´s belly full of big stones and threw him into the river.'''
+}, {
+    'name': 'GOSE',
+    'heroes': '',
+    'summary': '''
+Parents at work. During the game, the eldest daughter forgot about her brother
+and the swan geese carried him away to Baba Yaga. A stove, an apple tree,
+a milky river in the jelly banks are ready to show her the way, but she must
+respect them: eat a pie, an apple, jelly. But the girl does not want to: they
+have better food at home. She made her way to Baba Yaga's hut. She made her spin.
+The mouse for a spoonful of porridge advises her to run away with her brother.
+Geese-swans in pursuit. Here, the girl gratefully eats everything that the river,
+the apple tree and the oven offer her, if only they would hide her and her brother.
+And so they were saved.''',
+    'struct': '''
+1) Absence (sisters asks her sister to look after her little brother)
+2) Prohibition (parents asked the girl to look after her little brother)
+3) Violation (sister began to play and her little brother got carried away by the geese)
+4) Finding out (The girl sees that the stove is standing and runs after the swan geese)
+5) Extradition (the girl asks the geese swans where they are going and where they are taking her brother)
+6) Trick (the swan geese says that they are taking the brother to their father, the sun, and that she can ask him about it)
+7) Aiding (The girl agrees to eat her rye pie)
+8) Wrecking (The geese swans are not going to the Sun, but to Baba Yaga)
+9) Mediation (Baba Yaga tells the girl that she has to weave a shirt, and she will give her her brother back)
+10) Departure (The girl goes to weave a shirt, having been told to do so by Baba Yaga)
+11) Victory (The girl weaves the shirt and Baba Yaga gives her her brother back)
+12) Elimination of misfortune or shortage (The girl got her brother back and returned home)''',
+    'text': '''Parents asked their daughter to look after her little brother, and she began to play.
+And the swan geese carried the boy away. The girl runs after her, she sees: the stove is standing.
+For answering the question where the evil birds are going, she asks to eat her rye pie.
+The spoiled girl did not eat a simple pie. I ran to the apple tree. She is ready to
+tell where the birds are, but first you need to eat her wild apple. The girl does not eat:
+at home they have plenty of orchard apples. It was the same with the milky river in the jelly banks:
+the girl said that her father had cream, and then he was reluctant to drink.
+
+She got to the hut on chicken legs. And there is Baba Yaga and her brother.
+The old woman told the girl to spin. Having received porridge from the girl,
+the mouse advises her to run away with her brother. So they did. Geese-swans in pursuit.
+This time, in order to hide them and save them, the girl respected everyone: she ate jelly,
+and an apple, and a pie. As she left, she thanked everyone for their help.
+And from the oven - immediately home. Soon the parents returned.''',
+}]
