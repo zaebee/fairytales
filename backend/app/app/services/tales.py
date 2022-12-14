7e0f9be3882673feeeeffe7c2ac2a1da63ae67c0
zaebee <zaebuntu@gmail.com>
2022-12-14 03:22:36 +0100

    [BE] Cleanup tales: removed mary tale, added mermaid.

--- backend/app/app/services/tales.py
+++ backend/app/app/services/tales.py
@@ -1,21 +1,24 @@
 TALES = [{
-    'name': 'MARY',
+    'name': 'MERMAID',
     'heroes': '',
-    'summary': (
-        'A fairy tale about a daughter who was cared for by the virgin. '
-        'The girl deceived her, and the Mother of God punished her for it.'),
+    'summary': '''
+The youngest of six mermaid princesses, the little mermaid is not only the most beautiful, but the most introspective and thoughtful. Since their mother has died, the mermaids are raised by their grandmother. The youngest asks for all the information she can get about the world above the sea. Each princess has her own sea garden, which she can decorate as she chooses. The youngest arranges everything in the shape of the sun, and places in it the statue of a handsome prince from a sunken ship.
+As each princess reaches age 16, she is allowed to swim to the surface and observe life above the waves. When it is her turn to visit the upper world, the little mermaid approaches the windows of a sailing ship and sees inside the prince whose statue she has kept in her garden. A terrible storm comes up, wrecking the ship and drowning all on board except the prince. He is saved by the mermaid, who places him safely on a distant shore. When some maidens and a princess discover him, the little mermaid flees.
+Her grandmother reveals the little mermaid can live on land, but at a terrible price. The sea witch has a potion that will transform her tail into legs and feet, but to get them, the little mermaid must give up her beautiful singing voice. And in order to gain a mortal soul, she must make the prince fall in love with and marry her.
+She agrees, and though each step on land is like walking on knives, she manages to join the court of the prince. Despite her muteness, the prince treats her kindly. One day the princess who found him on the shore arrives and the prince, recognizing her as the one who saved him from drowning, vows his love for her. Together with the little mermaid, they sail away to their wedding. Her sisters bring the little mermaid a knife bought from the sea witch with their hair, instructing her to murder the prince with it so she can rejoin them in the sea. But the little mermaid is unable to do this. Brokenhearted, she kisses the happy couple and dives at sunrise to die in the waves of the sea. Instead, she finds herself lifted out of the water by the daughters of the air, who teach her how to bless children and thereby obtain an eternal soul.''',
     'struct': '''
-1) Absence (the Virgin Mary has a long way to go. So, she gives the girls the keys to the thirteen gates of heaven)
-2) Prohibition (You can do twelve doors, but I forbid you to open the thirteenth door! Do not open it, otherwise you will be unhappy!)
-3) Violation (Once, when all the angels left, out of curiosity, the girl nevertheless opened the thirteenth door)
-4) Finding out (On her return, the Ever-Virgin looked into her eyes and asked: “Have you unlocked the thirteenth door too?” “No”)
-5) Issuance (Virgin Mary demanded the keys back)
-6) A catch (the Virgin Mary found out about the deception and said: “You disobeyed me and, in addition, you lied, and therefore you are not worthy to stay in heaven anymore!”)
-7) Aiding (The King continued: “Would you like to come with me to my castle?” To this she answered only with a slight nod of her head)
-8) Wrecking (the Blessed Virgin took three children, wanting to hear the truth and repentance)
-9) Mediation (I am the Virgin Mary, the mother of the infant Christ. You are poor, burdened with need. Bring me your child: I will take him with me, I will be his mother and take care of him)
-10) Sending (The woodcutter obeyed, brought his child and handed him to the Virgin Mary, who took him to heaven with her)
-11) Victory (The Queen repented that she opened the door. Then the voice returned to her, and she loudly confessed to Mary what she had done. And at the same moment it rained from heaven and extinguished the flame. The Virgin Mary descended to earth with her newborn daughter)''',
+1) Absence (The little mermaid asks her grandmother about the world above the sea)
+2) Prohibition (The grandmother tells the little mermaid that she should not go to the surface of the sea)
+3) Violation (The little mermaid goes to the surface of the sea)
+4) Finding out (The little mermaid sees the prince and falls in love with him)
+5) Extradition (The little mermaid finds out that the prince is on the ship and saves him from drowning)
+6) Trick (The sea witch offers the little mermaid the opportunity to become a human, but she must give up her beautiful voice)
+7) Aiding (The little mermaid agrees to become a human)
+8) Wrecking (The little mermaid becomes a human and lives in the palace with the prince)
+9) Mediation (The prince falls in love with the little mermaid)
+10) Departure (The prince and the little mermaid go to the wedding)
+11) Victory (The little mermaid cannot kill the prince)
+12) Elimination of misfortune or shortage (The little mermaid dies and becomes a daughter of the air)''',
     'text': '''
 A poor woodcutter and his wife had a three-year-old daughter that they could not feed.
 The Virgin Mary appeared to the woodcutter and promised to take care of the child, so they gave her the child.
@@ -36,65 +39,18 @@ his councilors, and the queen was condemned to death. When she was brought to th
 she could confess before she died. The Virgin Mary brought back her children, restored her the power of speech,
 and gave her happiness the rest of her life.'''
 }, {
-    'name': 'BUN',
-    'heroes': '''<character> Luke Skywalker <description> Luke Skywalker is the hero. A naive farm boy, he will discover
-special powers under the guidance of mentor Ben Kenobi. <stop>
-<character> Ben Kenobi <description> Ben Kenobi is the mentor figure. A recluse Jedi warrior, he will
-take Luke Skywalker as apprentice. <stop>
-<character> Darth Vader <description> Darth Vader is the antagonist. As a commander of the evil Galactic
-Empire, he controls space station The Death Star. <stop>
-<character> Princess Leia <description> Princess Leia is a feisty and brave leader of the Rebellion. She
-holds the plans of the Death Star. She will become Luke's friend. <stop>
-<character> Han Solo <description> Han Solo is a brash mercenary space pilot of the Millenium Falcon and
-a friend of Chebacca. He will take Luke on his spaceship. <stop>
-<character> Chewbacca <description> Chewbacca is a furry and trustful monster. He is a friend of Han
-Solo and a copilot on the Millemium Falcon. <stop>''',
-    'summary': (
-        'A fairy tale about a bun that ran away from grandparents, '
-        'ran away from all animals, but the fox ate it.'),
-    'struct': '''
-1) Absence (The old man wants to eat the bun)
-2) Prohibition (The old woman forbids the bun to go out to the windowsill)
-3) Violation (The bun goes out to the windowsill)
-4) Finding out (The hare says he wants to eat the bun)
-5) Extradition (The bun says he ran away from grandfather, grandmother, hare and wolf)
-6) Trick (The fox offers to hear the bun sing, and to eat him at the same time)
-7) Aiding (The bun agrees to sing for the fox)
-8) Wrecking (The bun sings and the fox eats him)
-9) Mediation (The bun runs away from home and meets the hare, wolf, bear, fox)
-10) Departure (The bun leaves home and runs along the road)
-11) Victory (The bun reaches the fox, the fox eats him)
-12) Elimination of misfortune or shortage (The fox eats the bun)''',
-    'text': '''
-One day the old man says to his wife ‘Please, bake me a bun’. The old
-woman takes some flour, some soar cream, some butter and some water,
-and makes a bun. She puts it on the windowsill to cool.
-But the bun cannot sit on the windowsill! It jumps from the windowsill to the
-bench, from the bench to the floor, from the floor to the door, and runs
-away.
-The bun runs along the road and meets a hare. ‘Little bun, little bun, I want
-to eat you!’ says the hare. ‘I ran away from Grandfather, I ran away from
-Grandmother. And I can run away from you, little hare!’ says the bun and
-runs away.
-The bun runs along the road and meets a wolf. ‘Little bun, little bun, I want
-to eat you!’ says the wolf. ‘I ran away from Grandfather, I ran away from
-Grandmother, I ran away from the hare. And I can run away from you, grey
-wolf!’ says the bun and runs away.
-The bun runs along the road and meets a bear. ‘Little bun, little bun, I want
-to eat you!’ says the bear. ‘I ran away from Grandfather, I ran away from
-Grandmother, I ran away from the hare, I ran away from the wolf. And I can
-run away from you, big bear!’ says the bun again and runs away.
-The bun runs along the road and meets a fox. ‘Little bun, little bun, I want to
-eat you!’ says the fox. ‘I ran away from Grandfather, I ran away from
-Grandmother, I ran away from the hare, I ran away from the wolf, I ran
-away from the bear. And I can run away from you, old fox!’
-‘What a nice song!’ says the fox. ‘But little bun, I’m old and I cannot hear
-you well. Sit on my nose and sing your song again.’ The bun jumps on the
-fox’s nose and … the fox eats it!'''
-
-}, {
     'name': 'RED_HOOD',
-    'heroes': '',
+    'heroes': '''
+<character> Red Riding Hood <description> Red Riding Hood is a little girl who got her nickname because of her beanie. A good, little girl lives with her mother. I didn’t listen to my mother’s advice and went to my grandmother through the forest. <stop>
+<character> Wolf <description> Wokf is the negative hero, dangerous predator.
+An evil and bloodthirsty beast, a cunning and clever deceiver. A greedy and insatiable predator, through cunning and deceit, found out my grandmother's address. <stop>
+<character> Mother <description> Mother is the working middle-aged woman.
+A caring woman, worried about her mother's health, sends gifts to her. <stop>
+<character> Grandma <description> Grandma is the  old sick woman.
+A helpless old woman who loves her granddaughter very much. She does not hear and see well, so she falls for the deception of the wolf. <stop>
+<character> Lumberjacks <description> Lumberjacks are workers who are engaged in logging.
+Courageous and strong lumberjacks who were passing by heard a noise, jumped into the hut and killed the wolf, freed the grandmother and granddaughter from his belly. <stop>
+''',
     'summary': (
         'The tale tells of a young girl in a red cap who walks through a dark '
         'forest to her grandmother\'s house. When she arrives, she discovers '
@@ -127,44 +83,4 @@ he could arrive before her and eat her grandmother. Then he ate Little red Ridin
 Later, after eating them, the wolf fell asleep beside the river. Suddenly, a woodcutter
 saw him and took the little girl and her granny out of the wolf´s stomach and saved their lives.
 Then he filled the wolf´s belly full of big stones and threw him into the river.'''
-}, {
-    'name': 'GOSE',
-    'heroes': '',
-    'summary': '''
-Parents at work. During the game, the eldest daughter forgot about her brother
-and the swan geese carried him away to Baba Yaga. A stove, an apple tree,
-a milky river in the jelly banks are ready to show her the way, but she must
-respect them: eat a pie, an apple, jelly. But the girl does not want to: they
-have better food at home. She made her way to Baba Yaga's hut. She made her spin.
-The mouse for a spoonful of porridge advises her to run away with her brother.
-Geese-swans in pursuit. Here, the girl gratefully eats everything that the river,
-the apple tree and the oven offer her, if only they would hide her and her brother.
-And so they were saved.''',
-    'struct': '''
-1) Absence (sisters asks her sister to look after her little brother)
-2) Prohibition (parents asked the girl to look after her little brother)
-3) Violation (sister began to play and her little brother got carried away by the geese)
-4) Finding out (The girl sees that the stove is standing and runs after the swan geese)
-5) Extradition (the girl asks the geese swans where they are going and where they are taking her brother)
-6) Trick (the swan geese says that they are taking the brother to their father, the sun, and that she can ask him about it)
-7) Aiding (The girl agrees to eat her rye pie)
-8) Wrecking (The geese swans are not going to the Sun, but to Baba Yaga)
-9) Mediation (Baba Yaga tells the girl that she has to weave a shirt, and she will give her her brother back)
-10) Departure (The girl goes to weave a shirt, having been told to do so by Baba Yaga)
-11) Victory (The girl weaves the shirt and Baba Yaga gives her her brother back)
-12) Elimination of misfortune or shortage (The girl got her brother back and returned home)''',
-    'text': '''Parents asked their daughter to look after her little brother, and she began to play.
-And the swan geese carried the boy away. The girl runs after her, she sees: the stove is standing.
-For answering the question where the evil birds are going, she asks to eat her rye pie.
-The spoiled girl did not eat a simple pie. I ran to the apple tree. She is ready to
-tell where the birds are, but first you need to eat her wild apple. The girl does not eat:
-at home they have plenty of orchard apples. It was the same with the milky river in the jelly banks:
-the girl said that her father had cream, and then he was reluctant to drink.
-
-She got to the hut on chicken legs. And there is Baba Yaga and her brother.
-The old woman told the girl to spin. Having received porridge from the girl,
-the mouse advises her to run away with her brother. So they did. Geese-swans in pursuit.
-This time, in order to hide them and save them, the girl respected everyone: she ate jelly,
-and an apple, and a pie. As she left, she thanked everyone for their help.
-And from the oven - immediately home. Soon the parents returned.''',
 }]
