fdc55bac52dce1bfd0f920d20926913d7aad846a
zaebee <zaebuntu@gmail.com>
2022-12-16 00:21:22 +0100

    [BE] Added prompt for title generation.

---
+++ backend/app/app/services/prompts/title.py
@@ -0,0 +1,36 @@
+TITLE = """Examples of alternative, original and descriptive titles for known
+fairy tales and stories.
+
+Example 1. Cinderella grew up with an evil stepmother and stepsisters. They would not let
+her go to the prince's ball, but her fairy godmother made it happen with magic.
+Cinderella danced with the prince and they fell in love. Since she had to leave
+by midnight, she ran and lost one slipper. The prince found the slipper and
+searched for her. After he found her, they were married and lived happily ever after.
+Title: Cinderella <end>
+
+Example 2. A frog asked a princess why she was crying. She replied that she had
+dropped her golden ball into the pond. The princess promised anything if he
+would get it for her. The frog got the ball for her, and asked to eat from her
+plate, live in the castle, and that she be his friend. She agreed, and the frog
+turned into a prince as he had been under a spell from a witch. They married and
+lived happily ever after.
+Title: Frog Prince <end>
+
+Example 3. Goldilocks saw a cabin in the woods and entered. There, she found
+three bowls of porridge. One was too cold, one too hot and the other just right,
+so she ate that one. Goldilocks then found three chairs, one too hard, one too
+soft, and the little one was just right and she sat in it and broke it. Of the
+three beds, one was too hard, one too soft and one just right, so she fell asleep.
+The bears came home and frightened her and she ran away.
+Title: Goldilocks and the Three Bears <end>
+
+Example 4. Hansel and Gretel's wicked stepmother took them into the forest and
+left them there. Soon, the children came upon a house made of sweets and they
+ate some. The evil witch that lived there locked them in a cage and was going
+to make a soup out of them. Gretel got out of the cage and pushed the witch into
+the boiling water. The children found treasure at the house. They took it home
+and they were never hungry again.
+Title: Hansel and Gretel <end>
+
+Example 5. {LOG_LINE}
+Title:"""
